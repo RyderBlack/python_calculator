cc165b06ab5e9e3ca69e8b0b536768fe3595acd3
rydeblack <ryhad.boughanmi@gmail.com>
2025-01-15 12:46:52 +0100

    refactor: refactor code with switch case and remove calculations func

--- main.py
+++ main.py
@@ -1,32 +1,12 @@
 import re
 
-# Add
-def addit(num1,operator, num2):
-    result = num1 + num2
+global cmd_history
+cmd_history = []
+
+
+def print_result(num1, operator, num2, result):
     print(f"{num1} {operator} {num2} = {result}")
     return result
-
-# Substract
-def substractor(num1, operator, num2):
-    result = num1 - num2
-    print(f"{num1} {operator} {num2} = {result}")
-    return result
-
-#Multiply
-def multiplicator(num1, operator, num2):
-    result = num1 * num2
-    print(f"{num1} {operator} {num2} = {result}")
-    return result
-
-#Divide
-def dividordef(num1, operator, num2):
-    try:
-        result = num1 / num2
-        print(f"{num1} {operator} {num2} = {result}")
-        return result
-    except ZeroDivisionError as e:
-        print("Error: Cannot divide by zero")
-        return None
 
 #Main function - Calculator
 def calc():
@@ -34,20 +14,16 @@ def calc():
         try:
             
             user_choice = input("Enter your calculation (or 'q' to quit): ").replace(" ", "")
-            
-            
+                      
             # let the user exit the program
             if user_choice.lower() == "q":
                 print("Exiting the calculator. Thank you!")
                 break
             
-            # parts = re.findall(r"[-+]?(?:\d*\.*\d+)", user_choice)
-            
-             
-            parts = re.split('([+-/*])', user_choice)
-            
-            
+            parts = re.split('([+-/*])', user_choice)        
             # print(parts)
+            cmd_history.append(parts)
+            print(cmd_history)
 
             if len(parts) != 3:
                 print("Invalid format. Please use: number operator number")
@@ -57,30 +33,36 @@ def calc():
             operator = parts[1]
             num2 = int(parts[2])
     
-                
-            
-            
-            if operator == "+":
-                # print("type of ", num1, num2)
-                addit(num1, operator, num2)
-            elif operator == "-":
-                substractor(num1, operator, num2)
-            elif operator == "*":
-                multiplicator(num1, operator, num2)
-            elif operator == "/":
-                dividordef(num1, operator, num2)
-            else:
-                print("enter a valid operation.")
+            match operator:
+                case "+":
+                    result = num1 + num2
+                    print_result(num1, operator, num2, result)
+                case "-":
+                    result = num1 - num2
+                    print_result(num1, operator, num2, result)
+                case "*":
+                    result = num1 * num2
+                    print_result(num1, operator, num2, result)
+                case "/":
+                    try:
+                        result = num1 / num2
+                        print_result(num1, operator, num2, result)
+                    except ZeroDivisionError:
+                        print("Error: Cannot divide by zero")
+                        # return None
+                case _:
+                    print("enter a valid operation.")
                 
                 
         except ValueError as e:
             print(f"Error: {e}. Please enter valid numbers.")
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
+        except KeyboardInterrupt:
+            print("Goodbye!")
+            return None
             
-        # to ask again for a new calc
-        # print("\nEnter another calculation or 'q' to quit")
-                
+            
 
 if __name__ == "__main__":
     calc()
